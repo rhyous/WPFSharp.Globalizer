119144849f0a6264d618d0e23167978910db3734
rhyous <rhyous@yahoo.com>
2015-05-14 03:16:43 -0600

    Added NuGet Solution

---
+++ WPFSharp.Globalizer.NuGet/content/App.xaml.pp
@@ -0,0 +1,9 @@
+<globalizer:GlobalizedApplication x:Class="$rootnamespace$.App"
+             xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+             xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+             xmlns:globalizer="clr-namespace:WPFSharp.Globalizer;assembly=WPFSharp.Globalizer"
+             StartupUri="Example\GlobalizedWindow.xaml">
+    <Application.Resources>
+         
+    </Application.Resources>
+</globalizer:GlobalizedApplication>
