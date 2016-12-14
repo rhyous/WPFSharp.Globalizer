28ed73d7059cbe201239fe292412a9119bdc2834
rhyous <rhyous@yahoo.com>
2016-12-14 00:02:21 -0700

    versioned to 1.0.5

--- WPFSharp.Globalizer/Properties/AssemblyInfo.cs
+++ WPFSharp.Globalizer/Properties/AssemblyInfo.cs
@@ -21,5 +21,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("26260edf-a6b3-4c8a-bb0c-712ad759abe3")]
 
-[assembly: AssemblyVersion("1.0.4.0")]
-[assembly: AssemblyFileVersion("1.0.4.0")]
+[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
